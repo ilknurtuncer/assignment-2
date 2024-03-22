db2288273ac167c7b59d3302e0d482219252c673
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2024-03-22 19:11:20 +0300

    getModelList

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,28 +3,64 @@
     EXPRESS - Store API
 ------------------------------------------------------- */
 
-const Products = require("../models/productsModel");
+const Categories = require("../models/categoryModel");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await Products.find();
+    const data = await res.getModelList(Categories);
     res.status(200).send({
       error: false,
+      details: await res.getModelListDetails(Categories),
+      // detailste geldi. detailse bana neler yapıldığını getiriyor.
+      // "search": {},
+      // "sort": {},
+      // "skip": 0,
+      // "limit": 30,
+      // "page": 0,
+      // "pages": false,
+      // "totalRec
       data,
     });
   },
   create: async (req, res) => {
-    const data = await Products.create(req.body);
+    // burda mongoose kendi hata forlatıyor. Bizim yazmamıza gerek yok.
+    const data = await Categories.create(req.body);
     res.status(201).send({
       error: false,
       body: req.body,
       data,
     });
   },
+
   read: async (req, res) => {
-    const data = await Products.findOne({ _id: req.params.id });
+    const data = await Categories.findOne({ _id: req.params.id });
     res.status(202).send({
       error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    // süslü içerisinde olmaz. default hali süslü, ilk alan koşul, ikinci alan yeni veri - süslü parantezler sırası ile filter, query, options
+    const data = await Categories.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+      //- update işlemini şemaya uygun olarak yapar. Validasyon işlemi yapar. yoksa hata fırlatır.
+    });
+    // modified data,
+    const newData = await Categories.findOne({ _id: req.params.id });
+    // modifiedCount bu kısımlar datanın içeriisnden kendisi geliyor. 209 hata vermesine , eğer güncellerse 1 güncellemezse 0 ama hata değil o nedenle aynı data gelmişse 209 hiç bişey olmadı. conflictsler 209
+    res.status(data.modifiedCount ? 202 : 209).send({
+      error: false,
+      newData,
+      data,
+    });
+  },
+  delete: async (req, res) => {
+    // ! modellerin isimlerini tekil vermek daha iyi
+
+    const data = await Categories.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
       data,
     });
   },
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,20 +9,31 @@ const User = require("../models/userModel");
 const passwordEncrypt = require("../helpers/passwordEncrypte");
 
 module.exports = {
-  list: async (req, res) => {
-    const data = await User.find();
-    res.status(200).send({
-      error: false,
-      data,
-    });
-  },
-  read: async (req, res) => {
-    const data = await User.findOne({ _id: req.params.id });
-    res.status(202).send({
-      error: false,
-      data,
-    });
-  },
+  // ? buradaki CRUD işlemleri userları listeler, günceller
+  // list: async (req, res) => {
+  //   const data = await User.find();
+  //   res.status(200).send({
+  //     error: false,
+  //     data,
+  //   });
+  // },
+  // update: async (req, res) => {
+  //   const data = await User.updateOne({ _id: req.params.id }, req.body, {
+  //     runValidators,
+  //   });
+  // },
+  // read: async (req, res) => {
+  //   const data = await User.findOne({ _id: req.params.id }, req.body, {
+  //     runValidators: true,
+  //   });
+  //   const newData = await Products.findOne({ _id: req.params.id });
+  //   // modifiedCount bu kısımlar datanın içeriisnden kendisi geliyor. 209 hata vermesine , eğer güncellerse 1 güncellemezse 0 ama hata değil o nedenle aynı data gelmişse 209 hiç bişey olmadı. conflictsler 209
+  //   res.status(data.modifiedCount ? 202 : 209).send({
+  //     error: false,
+  //     newData,
+  //     data,
+  //   });
+  // },
 
   // ***  login - logout ***
 
@@ -31,10 +42,16 @@ module.exports = {
     if (email && password) {
       const user = await User.findOne({ email });
 
+      // ! login işleminde tekrar şifleme yapmalıyız.
       if (user && user.password == passwordEncrypt(password)) {
         // SESSION
         req.session.id = user.id;
+        console.log(req.session);
         req.session.password = user.password;
+        // burada role tanımlama işlemleri yapılacak. req.session.isAdmin = true -> cookie'i çözümlediğimiz yerde kullanıcı bilgilerine ekleyeceğiz. Permission middlewareı olacak admin mi değil mi ona bakacağız.
+        // burada password ve id almak yerine kullanıcın diğer bilgilerini almak daha güvenlidir. Sonra istek attığımızda headers kısmından bu bilgileri tekrar çekip kullanabiliriz. Cookie vs. işlemleri için.
+
+        // req.session.role -> o esnadaki kullanıcın rolü buraya eklenecek.
 
         if (req.body?.remindMe) {
           req.session.remindMe = req.body.remindMe;
