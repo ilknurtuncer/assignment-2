d66260202ba1c98d601a327cb1e1103c31ca5a3a
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2024-03-22 19:17:25 +0300

    isAdmin

---
+++ src/middlewares/permission.js
@@ -0,0 +1,12 @@
+module.exports = {
+    isAdmin: (req, res, next) => {
+      // ? req.user bizim userControl middlewareinden global olarak geliyor. Orda findOne ile bulduk onu.
+      if (req.user && req.user.isAdmin) {
+        console.log(req.user.isAdmin);
+        next();
+      } else {
+        // res.errorStatusCode = 403;
+        throw new Error("No Permission: You must be admin");
+      }
+    },
+  };
